0cbb409f907f190d5a11abf75ed88ce39b02d00e
exkungen <23136@ma-web.nl>
2018-02-23 09:20:42 +0100

    sdfs
    sdf

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var PDK = require('node-pinterest');
 var pinterest = PDK.init('AV5nkJsfoYv0-5wgrzwCUwTpVuNHFRVJ4QjNuH5EuIIz3IBGzgAAAAA');
-var bodyParser = require('body-parser');
 var mysql = require('mysql');
-const parseJson = require('parse-json');
 
 
 
@@ -11,8 +9,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: '23136_database',
-    port: 3307
+    database: '23136_database'
 });
 
 
@@ -21,58 +18,38 @@ connection.connect(function(err) {
         throw err;
     else {
         console.log('Connected to MySQL');
-        // Start the app when connection is ready
+
 
 
     }
 
 
-// pinterest.api('me').then(console.log);
+
 
     var options = {
         qs: {
             fields: "id,url,created_at,note",
-
-            limit: 1
+            limit: 100
 
 
         }
 
     };
-// var pins = [];
-// console.log(options.qs.fields);
+
     pinterest.api('me/pins', options).then(function (json) {
 
         var stringify = JSON.stringify(json.data);
-        // console.log(json);
         var parsed = JSON.parse(stringify);
 
 
-//
-//
+
         for (var i = 0; i < parsed.length; i++) {
 
-            var array = [];
-
-            array.push(parsed[i].url);
-
-
-             var values =  '[' + '\''  +array+ '\'' + ']' +',';
-
-
-            // var splice = array.splice(0 , 999 , "[","]");
-              console.log(values);
-
-
-            // var values = [['https://www.pinterest.com/pin/523895369145026448/'],['https://www.pinterest.com/pin/523895369145026448/']];
-
-
-
-            var sql = "INSERT INTO test (url) VALUES =" +mysql.escape(values);
-            connection.query(sql, function (err, result) {
+            var sql = "INSERT INTO test (url) VALUES (?)";
+            connection.query(sql, parsed[i].url,  function (err, result ){
                 if (err) throw err;
                 console.log("Number of records inserted: " + result.affectedRows);
-            });
+            })
 
 
 
@@ -82,30 +59,5 @@ connection.connect(function(err) {
 
 });
 
-// pinterest.api('me/boards', options).then(console.log);
 
-// pinterest.api('me/pins', options).then(function(json) {
-//     // console.log(json.data);
-//     if (json) {
-//
-//         pinterest.api(json.page.next).then(function (json) {
-//             var apijson =  JSON.parse(json);
-//             console.log(apijson.id);
-//         });
-//     }
-// });
-
-// pinterest.api('me/boards').then(function(json) {
-//     pinterest.api('pins', {
-//         method: 'POST',
-//         body: {
-//             board: json.data[0].id,
-//             note: 'this is a test',
-//             link: 'http://gizmodo.com/amazon-prime-music-finally-gets-tunes-from-universal-mu-1733540468',
-//             image_url: 'http://i.kinja-img.com/gawker-media/image/upload/s--4Vp0Ks1S--/1451895062187798055.jpg'
-//         }
-//     }).then(function(json) {
-//         pinterest.api('me/pins').then(console.log);
-//     });
-// });
 
